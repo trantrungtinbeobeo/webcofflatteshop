b89941bc0849965b25aa7e9444fe6b87cdbd86fd
Tran Trung Tin <119839140+trantrungtinbeobeo@users.noreply.github.com>
2026-05-22 09:17:42 +0700

    Fix MVC view errors and implement responsive purple layout

--- webcofflatteshop/Program.cs
+++ webcofflatteshop/Program.cs
@@ -1,24 +1,30 @@
 using webcofflatteshop.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<IProductRepository,
-MockProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,
-MockCategoryRepository>();
+builder.Services.AddSingleton<IProductRepository, MockProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
+
 app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
